1d4e6678e5356a309ea83a7cca517f5602deef1d
Ubuntu <ubuntu@ip-172-31-4-215.us-west-2.compute.internal>
2013-07-06 23:43:06 +0000

    a

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
 
 
  var buffer = new Buffer("Hello World from index.html", 25);
-var data = fs.readFileSync("index.html", buffer.toString());  
+var data = fs.readFileSync("index.html", buffer.toString("utf-8"));  
 response.send(data);
   });
 
