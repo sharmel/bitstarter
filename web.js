6f50ee3c0663bdb4689469133896e53b46629f60
Ubuntu <ubuntu@ip-172-31-4-215.us-west-2.compute.internal>
2013-07-06 12:41:19 +0000

    test read from file

--- web.js
+++ web.js
@@ -1,19 +1,12 @@
-//node sample clone or fork from heroku url from lecture 1
-
-
-
-var express = require('express');
+var http = require('http');
 var fs = require('fs');
-var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+http.createServer(function(request, response) {
   response.writeHead(200);
 
-fs.readFile('index.html', function(err, contents) {
+  fs.readFile('index.html', function(err, contents) {
 
   });
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+  
+  response.end();
+}).listen(8080);
