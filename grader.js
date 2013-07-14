9443d320cd12b796106ec0f30996a662a0e702c0
Ubuntu <ubuntu@ip-172-31-4-215.us-west-2.compute.internal>
2013-07-14 09:12:57 +0000

    now accept both url and html file

--- grader.js
+++ grader.js
@@ -1,23 +1,24 @@
 #!/usr/bin/env node
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development and basic DOM parsing.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
+
 References:
 
-+ cheerio
- - https://github.com/MathewMueller/cheerio
- - https://encosina.com/cheerio-faster-windows-friendly-alternative-jsdom/
- - https://maxogden.com/scraping-with-node.html
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
 
-+ commander.js
- - https://github.com/visionmedia/commander.js
- - https://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
-+ JSON
-
- - https://en.wikipedia.org/wiki/JSON
- - https://developer.mozilla.org/en-US/docs/JSON
- - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
 var fs = require('fs');
@@ -26,52 +27,84 @@ var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
-      var assertFileExists = function(infile) {
-	  var instr = infile.toString();
-	if(!fs.existsSync(instr)) {
-	    console.log("%s does not exist. Exiting.", instr);
-	    process.exit(1); //http://nodejs.org/api/process.html#process_process_exit_code
-	  }
-	  return instr;
+//URL SUPPORT
+
+var URL_DEFAULT = "";
+var rest = require('restler');
+
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var assertURLValid = function(infile) {
+    var instr = infile.toString();
+    return instr;
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-var checks = loadChecks(checksfile).sort();
-
-var out = {};
+var cheerioProcess = function(checksfile, htmldata) {
+    $ = cheerio.load(htmldata);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
     for(var ii in checks) {
-	var present = $(checks[ii]).length > 0;
-	out[checks[ii]] = present;
-}
-return out;
-
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return (out);
 };
 
-var clone = function(fn) {
+var printtoconsole = function(out) {
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
+};
 
-    // workaround for commander.js issue.
+var checkHtml = function(htmlfile, checksfile, htmlurl) {
+  if(htmlurl=="") fs.readFile(htmlfile, "utf8", function(error, data) {
+    if (!(error)) {
+      var out = cheerioProcess(checksfile, data);
+      if (require.main==module) printtoconsole (out);
+      return(out);
+    }
+    else {
+      console.log ('HTML input file reading error');
+      process.exit(1);
+    }
+  })
+  else rest.get(htmlurl).on('complete', function(result) {
+         if (result instanceof Error) {
+           console.log('URL reading error: ' + result.message);
+           process.exit(1);}
+         else {
+           var out = cheerioProcess(checksfile, result);
+           if(require.main==module) printtoconsole(out);
+           return(out);
+         }
+       });
+};
+
+
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
-if(require.main == module) { 
-
-    program
-             .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKFILEDEFAULT)
-             .option('-f, --file <html_file>' 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-              .parse(process.argv);
-        var checkJson = checkHtmlFile(program.file, program.checks);
-        var outJson = JSON.stringify(checkJson, null, 4);
-        console.log(outJson);
+if(require.main == module) {
+program
+    .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+    .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url_link>', 'URL to index.html', clone(assertURLValid), URL_DEFAULT)
+    .parse(process.argv);
+    checkHtml(program.file, program.checks, program.url);
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtml = checkHtml;
 }
