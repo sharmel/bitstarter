8a2f6479dd30331da6971f11ac9bb80b8b7d927c
Ubuntu <ubuntu@ip-172-31-4-215.us-west-2.compute.internal>
2013-07-14 18:17:06 +0000

    with port:8080

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ var data = fs.readFileSync("index.html", "utf-8");
 response.send(data);
   });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
