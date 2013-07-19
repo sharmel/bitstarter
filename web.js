90425b8f2e89f82f547ef9d595d43ff38d97ccf0
Ubuntu <ubuntu@ip-172-31-4-215.us-west-2.compute.internal>
2013-07-19 16:07:15 +0000

    image link added

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-app.use(express.static(_dirname + '/images'));
+app.use(express.static(__dirname + '/images'));
 
 
 // var buffer = new Buffer("Hello World from index.html", 25);
